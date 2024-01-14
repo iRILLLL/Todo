6f6f41dca33f263578075f28fb0e1bebdc025aaf
iril <hello@iril.dev>
2024-01-14 12:12:02 +0700

    request push notification permission

--- features/Sources/TodoUI/TodoDetailView.swift
+++ features/Sources/TodoUI/TodoDetailView.swift
@@ -3,10 +3,12 @@ import TodoInterface
 
 public struct TodoDetailView: View {
     
+    @State private var viewModel: ViewModel
     private let todo: Todo
     private var mutatedTodo: Todo
     
     public init(todo: Todo) {
+        _viewModel = State(initialValue: ViewModel())
         self.todo = todo
         self.mutatedTodo = todo
     }
@@ -22,14 +24,18 @@ public struct TodoDetailView: View {
                 TextField(todo.name, text: $name)
             }
             Section {
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Image(systemName: "bell")
-                        Text("Remind Me")
+                
+                if !viewModel.isPushNotificationAuthorized {
+                    Button {
+                        viewModel.askPushNotificationPermission()
+                    } label: {
+                        HStack {
+                            Image(systemName: "bell")
+                            Text("Remind Me")
+                        }
                     }
                 }
+                
                 Button {
                     withAnimation {
                         showDatePicker.toggle()
@@ -95,6 +101,52 @@ public struct TodoDetailView: View {
         .onAppear {
             if todo.name != "Todo" {
                 name = todo.name
+            }
+        }
+    }
+}
+
+extension TodoDetailView {
+    
+    @Observable
+    final class ViewModel {
+        
+        private let center = UNUserNotificationCenter.current()
+        
+        private(set) var isPushNotificationAuthorized: Bool = false
+        
+        init() {
+            self.checkPushNotificationPermission()
+        }
+        
+        func checkPushNotificationPermission() {
+            center.getNotificationSettings { settings in
+                switch settings.authorizationStatus {
+                case .notDetermined:
+                    self.isPushNotificationAuthorized = false
+                case .denied:
+                    self.isPushNotificationAuthorized = false
+                case .authorized:
+                    self.isPushNotificationAuthorized = true
+                case .provisional:
+                    self.isPushNotificationAuthorized = true
+                case .ephemeral:
+                    self.isPushNotificationAuthorized = true
+                @unknown default:
+                    self.isPushNotificationAuthorized = false
+                }
+            }
+        }
+        
+        func askPushNotificationPermission() {
+            center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+                
+                if let error {
+                    print(error)
+                    return
+                }
+                
+                self.isPushNotificationAuthorized = granted
             }
         }
     }
